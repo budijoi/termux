f66a7dc0e9bb6c34988f47673c692968c72d425c
MOCH84BDG <91376150+budijoi@users.noreply.github.com>
2022-11-19 23:08:29 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,9 +4,24 @@ clear
 echo -e "================================================="
 echo -e "#             Installing Speedtest              #"
 echo -e "================================================="
-git clone https://github.com/thelearn-tech/speedtest-cli && cd speedtest-cli && chmod +x install.sh && ./install.sh
+clear
+cd $HOME
+echo "Installing......."
+sleep 2
+curl -Lo speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
+chmod +x speedtest-cli
+cp speedtest-cli  speedtest
+chmod +x speeedtest
+mv speedtest $PREFIX/bin
+rm -rf speedtest-cli
+echo "SpeedTest installed"
+sleep 2
+echo ""
+echo " type speedtest to begin or ./speedtest"
+sleep 1
+exit 1
 echo -e "================================================="
 echo -e "#              Speedtest Installed              #"
 echo -e "================================================="
 echo -e ""
-echo -e "    Ketik menu untuk kembali ke menu utama       "
+echo -e "Ketik menu untuk kembali ke menu utama"
