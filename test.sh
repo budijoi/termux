d869118dc9b35768d7fa909ba6f7da54b7580c1f
MOCH84BDG <91376150+budijoi@users.noreply.github.com>
2022-11-19 23:10:41 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,24 +4,10 @@ clear
 echo -e "================================================="
 echo -e "#             Installing Speedtest              #"
 echo -e "================================================="
-clear
-cd $HOME
-echo "Installing......."
-sleep 2
-curl -Lo speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
-chmod +x speedtest-cli
-cp speedtest-cli  speedtest
-chmod +x speeedtest
-mv speedtest $PREFIX/bin
-rm -rf speedtest-cli
-echo "SpeedTest installed"
-sleep 2
-echo ""
-echo " type speedtest to begin or ./speedtest"
-sleep 1
-exit 1
+pkg install python -y
+pip install speedtest-cli
 echo -e "================================================="
 echo -e "#              Speedtest Installed              #"
 echo -e "================================================="
 echo -e ""
-echo -e "Ketik menu untuk kembali ke menu utama"
+echo -e "Ketik speedtest untuk memulai tes kecepatan"
