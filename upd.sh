0ddaa9db663c017acfde5479fdd6d7ebaf196530
MOCH84BDG <91376150+budijoi@users.noreply.github.com>
2022-11-19 23:04:24 +0700

    Update upd.sh

--- upd.sh
+++ upd.sh
@@ -6,6 +6,9 @@ echo -e "#          Update and Upgrade Dimulai            #"
 echo -e "================================================="
 pkg update -y
 pkg upgrade -y
+pkg install wget -y
+pkg install git -y
+pkg install figlet -y
 echo -e "================================================="
 echo -e "#          Update and Upgrade Selesai            #"
 echo -e "================================================="
