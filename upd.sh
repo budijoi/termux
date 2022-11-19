a47e5915af097f3792e4d8095e33bb3f23f7cc50
MOCH84BDG <91376150+budijoi@users.noreply.github.com>
2022-11-19 22:10:25 +0700

    Create upd.sh

---
+++ upd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#          Update and Upgrade Dimulai            #"
+echo -e "================================================="
+pkg update -y
+pkg upgrade -y
+echo -e "================================================="
+echo -e "#          Update and Upgrade Selesai            #"
+echo -e "================================================="
+echo -e "Termux Script By MOCHVPN"
